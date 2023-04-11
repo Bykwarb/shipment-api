6f13d61649ab528c179610c74d771c5def29d5e9
Bykwarb <nebykwarb@gmail.com>
2023-04-11 10:09:10 +0300

    create shipment api

--- api/api.go
+++ api/api.go
@@ -1,1 +1,13 @@
 package api
+
+import (
+	"task/iternal/shipments"
+)
+
+type shipmentApi struct {
+	service *shipments.ShipmentService
+}
+
+func NewShipmentApi(service shipments.ShipmentService) *shipmentApi {
+	return &shipmentApi{&service}
+}
